726466f1d0f97f6ff2b5269af6836e6f45b51f22
heyitzzmanoj <sutharmanoj275@gmail.com>
2026-08-01 08:28:52 +0530

    Refactor and clean up main.py code

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ dates = str(month)+"-"+str(day)
 bd_list = bd.date.to_list()
 if dates in bd_list:
     d = bd[bd.date==dates] 
-    email=d.email.item()
+    email=d.email
     name = d.name.item()
     with open("massage.txt") as f:
         d= f.read()
